362d69e4bfaf37270141daf83f0134619c4c7f93
rootbmb <rootbmb@gmail.com>
2017-12-13 12:11:10 +0300

    Add config file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from time import sleep
 import serial
 import csv
+import configparser
 
 
 def pars_csv(filename):
@@ -14,27 +15,44 @@ def pars_csv(filename):
         return out_list
 
 
-def send_msg(num, msg):
-    sleep(10)
-    device = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)
-    device.write('ATZ\r'.encode())
-    device.write('AT+CMGF=1\r\n'.encode())
-    device.write('AT+CSCS="UTF-8"\r\n'.encode())
-    device.write('AT+CSMP=17,167,0,8\r\n'.encode())
-    device.write('AT+CMGS="{}"\r\n'.format(num).encode())
-    device.write(msg)
-    device.write(chr(26).encode())
-    device.close()
+def send_msg(num=None, msg=None, dev=None):
+    if num != "Close":
+        sleep(10)
+        dev.write('ATZ\r'.encode())
+        dev.write('AT+CMGF=1\r\n'.encode())
+        dev.write('AT+CSCS="UTF-8"\r\n'.encode())
+        dev.write('AT+CSMP=17,167,0,8\r\n'.encode())
+        dev.write('AT+CMGS="{}"\r\n'.format(num).encode())
+        dev.write(msg)
+        dev.write(chr(26).encode())
+    else:
+        print("Close connecting modem")
+        dev.close()
+
+
+def conf_get(filename):
+    conf = configparser.ConfigParser()
+    conf.read(filename)
+    port = conf.get("serial", "port")
+    baudrate = conf.get("serial", "baudrate")
+    timeout = conf.get("serial", "timeout")
+    return port, baudrate, timeout
 
 
 def main():
+    config_filename = "config.ini"
+    conf = conf_get(config_filename)
+    dev = serial.Serial(conf[0], int(conf[1]), timeout=int(conf[2]))
     msg = ''.join('%04X' % ord(c) for c in 'Привет')
     msg = msg.encode('utf-8')
     csv_file = 'test.csv'
     out_list = pars_csv(csv_file)
+
     for i in out_list:
         num = '+' + str(i)
-        send_msg(num, msg)
+        send_msg(num, msg, dev)
+    else:
+        send_msg(num="Close", dev=dev)
 
 
 if __name__ == '__main__':
